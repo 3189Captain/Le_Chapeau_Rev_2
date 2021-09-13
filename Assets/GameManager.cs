64a9342181c54d53144082a8a2130dce931142a7
3189Captain <oogabooga300@gmail.com>
2021-09-13 09:23:04 -0700

    initial commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Linq;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    [Header("Stats")]
+    public bool gameEnded = false;
+    public float timeToWin;
+    public float invincibleDuration;
+    private float hatPickupTime;
+
+    [Header("Players")]
+    public string playerPrefabLocation;
+    public Transform[] spawnPoints;
+    public PlayerController[] players;
+    public int playerWithHat;
+    private int playersInGame;
+
+    //instance the game mananger
+    public static GameManager instance;
+    private void Awake()
+    {
+        //lazy singleton, change later
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        players = new PlayerController[PhotonNetwork.PlayerList.Length];
+        photonView.RPC("ImInGame", RpcTarget.All);
+    }
+
+    [PunRPC]
+    void ImInGame()
+    {
+        ++playersInGame;
+        if (playersInGame == PhotonNetwork.PlayerList.Length)
+            SpawnPlayer();
+    }
+
+    void SpawnPlayer()
+    {
+        GameObject playerObj = PhotonNetwork.Instantiate(playerPrefabLocation, spawnPoints[Random.Range(0, spawnPoints.Length - 1)].position, Quaternion.identity);
+
+        PlayerController playerScript = playerObj.GetComponent<PlayerController>();
+
+        playerScript.photonView.RPC("Initialize", RpcTarget.All, PhotonNetwork.LocalPlayer);
+    }
+
+    public PlayerController GetPlayer(int playerId)
+    {
+        return players.First(x => x.id == playerId);
+    }
+
+    public PlayerController GetPlayer(GameObject playerObject)
+    {
+        return players.First(x => x.gameObject == playerObject);
+    }
+
+    [PunRPC]
+    public void GiveHat(int playerId, bool initialGive)
+    {
+        if (!initialGive)
+            GetPlayer(playerWithHat).SetHat(false);
+
+        playerWithHat = playerId;
+        GetPlayer(playerId).SetHat(true);
+        hatPickupTime = Time.time;
+    }
+
+    public bool CanGetHat()
+    {
+        if (Time.time > hatPickupTime + invincibleDuration)
+            return true;
+        else
+            return false;
+    }
+
+    [PunRPC]
+    void UnleashThePower(int playerId)
+    {
+        GetPlayer(playerId).SetThePower(true);
+    }
+
+    [PunRPC]
+    void WinGame(int playerId)
+    {
+        gameEnded = true;
+        PlayerController player = GetPlayer(playerId);
+        GameUI.instance.SetWinText(player.photonPlayer.NickName);
+
+        Invoke("GoBackToMenu", 3.0f);
+    }
+
+    void GoBackToMenu()
+    {
+        PhotonNetwork.LeaveRoom();
+        NetworkManager.instance.ChangeScene("Menu");
+    }
+}
